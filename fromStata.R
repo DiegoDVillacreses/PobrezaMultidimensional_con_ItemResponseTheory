41a01b2bd283f43ded70a19eb390f8cc98cc938e
dvillacresesz <33496682+dvillacresesz@users.noreply.github.com>
2019-06-20 12:38:57 -0500

    Add files via upload

---
+++ fromStata.R
@@ -0,0 +1,10 @@
+enem18dic = read.dta13("201812_enemdu.dta", convert.factors = F)
+enem18dic.var.labels = attr(enem18dic,"var.labels")
+enem18dic.var.labels = data.table(VarName = names(enem18dic),
+                                  VarLabel = enem18dic.var.labels)
+enem18dic.val.labels = attr(enem18dic,"label.table")
+enem18dic = enem18dic %>% data.table()
+
+fwrite(x = enem18dic, file = "enem18dic.csv")
+fwrite(x = enem18dic.var.labels, file = "enem18dic.var.labels.csv")
+saveRDS(object = enem18dic.val.labels, file = "enem18dic.val.labels.RDS")
